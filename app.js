c466b4c3f41a5285a5ff698396c149ea86cd7c07
Nils Berglund <nilsberglund04@gmail.com>
2022-02-28 12:30:34 +0100

    glömt att göra ett repository tidigare men har gjort the basics för att listan ska fungera

---
+++ app.js
@@ -0,0 +1,25 @@
+function newtodo() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("todo").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+    } else {
+      document.getElementById("ul").appendChild(li);
+    }
+    document.getElementById("todo").value = "";
+  
+    var span = document.createElement("SPAN");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
+
+  
