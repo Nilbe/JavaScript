7ee14c57cb59a1d83d8e24b3d357f813c5d046aa
Nils Berglund <nilsberglund04@gmail.com>
2022-03-09 14:44:26 +0100

    commit

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-function newtodo() {
-        var li = document.createElement("li");
-    var inputValue = document.getElementById("todo").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-    } else {
-      document.getElementById("ul").appendChild(li);
-    }
-    document.getElementById("todo").value = "";
-  
-    var span = document.createElement("SPAN");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-  }
+let newtodoButton = document.getElementById("newtodo");
+let todo = document.getElementById("todo");
+let input = document.getElementById("input");
 
+newtodoButton.addEventListener('click', function(){
+  let paragraph = document.getElementById('p')
+  paragraph.innerText = input.nodeValue;
+  todo.appendChild(paragraph);
+
+})
